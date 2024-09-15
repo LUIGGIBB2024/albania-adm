500d2ed480dcb8e9386e7f0cc98a024744874416
Luis Alberto Bernal B. <enlacevisual@hotmail.com>
2024-09-15 10:57:16 -0500

    Update Send Beneficiarios

--- app/Http/Controllers/api/SendBeneficiariosController.php
+++ app/Http/Controllers/api/SendBeneficiariosController.php
@@ -5,15 +5,56 @@ use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Http\JsonResponse;
 
 use App\Http\Controllers\Controller;
+use App\Models\Beneficiario;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SendBeneficiariosController extends Controller
 {
     public function SendBeneficiarios(Request $request):JsonResponse
     {
         $contador = 0;
+        $equipo     = $request->equipo;
 
         $beneficiarios = json_decode($request->databeneficiarios);
+        
+        if (isset($request->databeneficiarios))
+        {
+            foreach($beneficiarios as $item)
+            {
+                $cedula     = $item->cedula;
+                DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+                        
+                Beneficiario::updateOrCreate(['cedula'=>$cedula,'equipo'=>$equipo],
+                [
+                    "tipodocumento"         => "CC",
+                    "estadoidentificacion"  => "",
+                    "nombres"               => $item->nombres,
+                    "apellidos"             => $item->apellidos,
+                    "direccion"             => $item->direccion,
+                    "telefono"              => $item->telefono,
+                    "email"                 => $item->email,
+                    "fechadenacimiento"     => $item->fechanac,
+                    "area"                  => "",
+                    "sexo"                  => $item->sexo,
+                    "contacto"              => "",
+                    "telefonocontacto"      => "",
+                    "gradodeescolaridad"    => $item->gradoescolaridad,            
+                    "poblacioninmigrante"   => "",
+                    "rutasfotos"            => "",
+                    "estado"                => "Activo",   
+                    "unidadadministrativa_id"   => 0,   
+                    "estrato_id"   => 0,    
+                    "zona_id"   => 0,     
+                    "barrio_id"   => 0,   
+                    "ciudad_id"   => 0,        
+                    "usuario_create" => "PHOENIX24",
+                    "usuario_update" => "PHOENIX24",
+                ]);       
+                DB::statement('SET FOREIGN_KEY_CHECKS=1;');   
+            }
+        }     
+    
         if ($contador > 0)
         {            
             return response()->json(
