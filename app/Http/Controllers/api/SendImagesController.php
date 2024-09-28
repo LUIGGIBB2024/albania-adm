e5a8ede9b242820846d2a309627fdd464af22b16
Luis Alberto Bernal B. <enlacevisual@hotmail.com>
2024-09-28 08:55:11 -0500

    Updated Storage Link

--- app/Http/Controllers/api/SendImagesController.php
+++ app/Http/Controllers/api/SendImagesController.php
@@ -14,12 +14,14 @@ class SendImagesController extends Controller
         $contador = 0;
 
         $dataimages = $request->dataimages;
+        $carpeta    = $dataimages->carpeta;
 
         return response()->json(
             [
             'status'    => '200 OK',
             'msg'       => 'No hubo actualización',
             'data'      => $dataimages,
+            'carpeta'      => $carpeta,
             //'encuestas' => $encuestas,
             ],Response::HTTP_ACCEPTED); 
 
