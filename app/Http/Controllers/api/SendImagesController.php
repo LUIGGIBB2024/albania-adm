ed30847cc6af8dd77477a21d0ebf81b26b524fc3
Luis Alberto Bernal B. <enlacevisual@hotmail.com>
2024-09-29 12:10:29 -0500

    Updated Storage Images

--- app/Http/Controllers/api/SendImagesController.php
+++ app/Http/Controllers/api/SendImagesController.php
@@ -21,14 +21,14 @@ class SendImagesController extends Controller
         //]);
 
         // Obtener el objeto dataimages del request
-        $carpeta = json_decode($request->dataimages);
+        $carpeta = json_decode($request);
         
 
         return response()->json(
             [
             'status'    => '202vOK 1992',
             'msg'       => 'Actualización Exitosa 999',
-            'carpeta'   => $carpeta->carpeta,                   
+            'carpeta'   => $carpeta,                   
             //'encuestas' => $encuestas,
             ],Response::HTTP_ACCEPTED);
 
