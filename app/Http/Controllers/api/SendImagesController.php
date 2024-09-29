fcd425a7d94fb748211158fc9e23ce90e54c3b7a
Luis Alberto Bernal B. <enlacevisual@hotmail.com>
2024-09-29 16:42:00 -0500

    Updated Storage Images

--- app/Http/Controllers/api/SendImagesController.php
+++ app/Http/Controllers/api/SendImagesController.php
@@ -30,7 +30,15 @@ class SendImagesController extends Controller
                 $paths = [];
                 if ($request->hasFile('imagen1')) {
                     $file1 = $request->file('imagen1');
-                    //var_dump($file1);
+                    
+                    return response()->json(
+                        [
+                        'status'   => '555555 OK',
+                        'msg'      => 'Error en el FOR',
+                        'file' => $file1,
+                        ],Response::HTTP_ACCEPTED);  
+
+                 
                     $filename1 = uniqid() . '.' . $file1->getClientOriginalExtension();
                     $path1 = $file1->storeAs('mis_imagenes/' . $carpeta, $filename1, 'public');
                     $paths['imagen1'] = Storage::url($path1);
