47c570a4a43f4b697c173501794ea3f8a1153308
Luis Alberto Bernal B. <enlacevisual@hotmail.com>
2024-11-03 15:59:32 -0500

    Updated DetalleDeEncuestas Get

--- app/Http/Controllers/api/GetDetalleDeEncuestasController.php
+++ app/Http/Controllers/api/GetDetalleDeEncuestasController.php
@@ -14,7 +14,7 @@ class GetDetalleDeEncuestasController extends Controller
 {
     public function GetDetalleDeEncuestas(Request $request):JsonResponse
     {          
-        $data           =  json_decode($request);
+        $data           =  $request->data;
         $desderegistro  =  $data->desdereg;   
         $hastaregistro  =  $data->hastareg;   
 
