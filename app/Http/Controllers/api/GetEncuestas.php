dd065b0b3cf9e867346df85da7b7a9d35afa99a8
Luis Alberto Bernal B. <enlacevisual@hotmail.com>
2024-11-04 22:06:16 -0500

    Updated Encuestas Get

--- app/Http/Controllers/api/GetEncuestas.php
+++ app/Http/Controllers/api/GetEncuestas.php
@@ -3,9 +3,29 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Encuesta;
 use Illuminate\Http\Request;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class GetEncuestas extends Controller
 {
-    //
+    public function GetEncuestas(Request $request):JsonResponse
+    { 
+        $encuestas  = Encuesta::All();
+        $contador   = Encuesta::Count();
+        if ($contador > 0)
+           {            
+                return response()->json(
+                [
+                    'status'      => '200 OK',
+                    'msg'         => 'Actualización Exitosa',   
+                    'data'        => $encuestas, 
+                    'contador'    => $contador,       
+                ],Response::HTTP_ACCEPTED);
+           } 
+    }
 }
+
+
