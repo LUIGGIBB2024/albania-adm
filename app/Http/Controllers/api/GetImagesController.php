dc871604f1d63626575bf4f7c917693f7ea7305a
Luis Alberto Bernal B. <enlacevisual@hotmail.com>
2025-01-30 22:36:54 -0500

    Updated GetImages ALBM

--- app/Http/Controllers/api/GetImagesController.php
+++ app/Http/Controllers/api/GetImagesController.php
@@ -15,16 +15,16 @@ class GetImagesController extends Controller
         
         $validator = $request->validate(['public/storage/encuestas/1.118.830.588-ALBADM-007' => 'required|url',]); 
 
-        //$errorvalidator = []; 
-        // if ($validator->fails()) {
-        //     // Return errors or redirect back with errors
-        //     $errorvalidator =  $validator->errors();
-        // }
+        $errorvalidator = []; 
+         if ($validator[0]->fails()) {
+             // Return errors or redirect back with errors
+             $errorvalidator =  $validator[0]->errors();
+         }
         return response()->json(
             [
             'status'    => 'OK',
             'msg'       => 'Error en el FOR',
-            'error'     => $validator[0]->errors(),
+            'error'     => $errorvalidator,
             ],Response::HTTP_ACCEPTED);
     }
 }
