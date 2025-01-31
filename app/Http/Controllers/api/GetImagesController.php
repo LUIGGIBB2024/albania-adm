ef51a9c3a84c9682c37834fd4aae36377adcbddc
Luis Alberto Bernal B. <enlacevisual@hotmail.com>
2025-01-30 23:36:12 -0500

    Updated GetImages ALBM

--- app/Http/Controllers/api/GetImagesController.php
+++ app/Http/Controllers/api/GetImagesController.php
@@ -13,14 +13,14 @@ class GetImagesController extends Controller
     public function GetImagesEncuestas(Request $request):JsonResponse
     {
         
-        //$validator = $request->validate([public_path('/storage/encuestas') => 'required|url',]); 
-        $validator = public_path('storage/encuestas'); 
+        $validator = $request->validate([public_path('storage/encuestas') => 'required|url',]); 
+        //$validator = public_path('storage/encuestas'); 
 
     
         return response()->json(
             [
             'status'    => 'OK',
-            'msg'       => 'Error en el FOR',
+            'msg'       => 'Envío Exitoso',
             'error'     => $validator,
             ],Response::HTTP_ACCEPTED);
     }
