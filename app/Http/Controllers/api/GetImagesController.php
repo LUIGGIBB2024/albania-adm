01fd7f9385d40466827bddbd98bb4c371a787b9d
Luis Alberto Bernal B. <enlacevisual@hotmail.com>
2025-01-30 22:30:52 -0500

    Updated GetImages ALBM

--- app/Http/Controllers/api/GetImagesController.php
+++ app/Http/Controllers/api/GetImagesController.php
@@ -6,21 +6,25 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\Storage;
+//use Illuminate\Support\Facades\Storage;
 
 class GetImagesController extends Controller
 {
     public function GetImagesEncuestas(Request $request):JsonResponse
     {
-        // $request->validate([
-        //     'public/storage/encuestas/1.118.830.588-ALBADM-007' => 'required|url',
-        // ]); 
+        
+        $validator = $request->validate(['public/storage/encuestas/1.118.830.588-ALBADM-007' => 'required|url',]); 
 
+        //$errorvalidator = []; 
+        // if ($validator->fails()) {
+        //     // Return errors or redirect back with errors
+        //     $errorvalidator =  $validator->errors();
+        // }
         return response()->json(
             [
-            'status'   => '555555 OK',
-            'msg'      => 'Error en el FOR',
-            //'error' => $ex->getMessage(),
+            'status'    => 'OK',
+            'msg'       => 'Error en el FOR',
+            'error'     => $validator[0]->errors(),
             ],Response::HTTP_ACCEPTED);
     }
 }
