36e8a0da697d5913049be71042956a191a510694
Luis Alberto Bernal B. <enlacevisual@hotmail.com>
2025-01-30 19:54:26 -0500

    Updated GetImages Albania

--- app/Http/Controllers/api/GetImagesController.php
+++ app/Http/Controllers/api/GetImagesController.php
@@ -4,8 +4,20 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Storage;
 
 class GetImagesController extends Controller
 {
-    //
+    public function GSendImagesEncuestasImagesEncuestas(Request $request):JsonResponse
+    {
+         
+        return response()->json(
+            [
+            'status'   => '555555 OK',
+            'msg'      => 'Error en el FOR',
+            //'error' => $ex->getMessage(),
+            ],Response::HTTP_ACCEPTED);
+    }
 }
